757dc930ac8b5de36d7a01a724b88889f7adf1b0
momo9 <liangchenchi@gmail.com>
2014-03-13 00:37:37 +0800

    quick sort, mid-pivot implementation

--- chap07/quick_sort.rb
+++ chap07/quick_sort.rb
@@ -1,11 +1,11 @@
-def quick_sort(a, p, r)
+def quick_sort(a, p, r, pivot_method)
 #	print [p, r], "\n"
 #	print a,"\n"
 #	gets.chomp()
 	if r-p>1
-		q = get_pivot(a, p, r)
-		quick_sort(a, p, q)
-		quick_sort(a, q, r)
+		q = pivot_method.call(a, p, r)
+		quick_sort(a, p, q, pivot_method)
+		quick_sort(a, q, r, pivot_method)
 	end
 end
 
@@ -22,39 +22,84 @@ def get_pivot(a, p, r)
 	return i+1
 end
 
-def get_pivot_mid(a, p, r)
+def get_pivot_bi(a, p, r)
 	i1 = p
-	i2 = r-1
-	q = (p+r)/2
+	i2 = r-2
+	pivot = a[r-1]
 	while true
-		while i1<r and a[i1] <= a[q]
+		while i1<r-1 and a[i1] <= pivot
 			i1 += 1
 		end
-		while i2>=p and a[i2] >= a[q]
+		while i2>=p and a[i2] >= pivot
 			i2 -= 1
 		end
 		
 		if i2<i1
-			if i1<=q
-				a[q],a[i1] = a[i1],a[q]
+			a[i1],a[r-1] = a[r-1],a[i1]
+			return i1
+		else
+			a[i1],a[i2] = a[i2],a[i1]
+		end
+	end
+end
+
+def get_pivot_mid(a, p, r)
+	# i1 and i2 should move at the same time
+	i1 = p
+	i2 = r-2
+	pivot = a[r-1]
+	while true
+		while true
+			if  i1 < r-1 and a[i1] <= pivot
+				i1 += 1
+			end
+			if i2 >= p and a[i2] >= pivot
+				i2 -= 1
+			end
+			
+			if i2<i1
+				a[i1],a[r-1] = a[r-1],a[i1]
 				return i1
-			elsif i2>=q
-				a[q],a[i2] = a[i2],a[q]
-				return i2
-			else
-				return q
+			end
+			
+			if a[i1] > pivot and a[i2] < pivot
+				a[i1],a[i2] = a[i2],a[i1]				
 			end
 		end
 	end
 end
 
-a = []
-for i in (0...10)
-	a.push(rand(20))
+
+def check(a,mid)
+	logic1 = true
+	logic2 = true
+	a[0...mid].each do |i|
+		if i>a[mid]
+			logic1 = false		
+		end
+	end
+	
+	a[mid...a.length()].each do |i|
+		if i<a[mid]
+			logic2 = false
+		end
+	end
+	return (logic1 and logic2)
 end
-#print a,"\n"
-#quick_sort(a,0,a.length())
-#print a
-#a = [5]*10
-puts get_pivot_mid(a,0,a.length())
-print a,"\n"
+
+a = [5]*10
+
+puts get_pivot_mid(a, 0, a.length())
+
+while true
+	a = []
+	for i in (0...15)
+		a.push(rand(20))
+	end
+
+	print a,"\n"
+	quick_sort(a, 0, a.length(), method(:get_pivot_mid))
+	print a
+	
+	gets.chomp()
+end
